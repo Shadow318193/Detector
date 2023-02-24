42eca98a56427e2c6a96dac4ef97c5c3e846ff0d
prokol <egor.kuzovkin.zr@gmail.com>
2023-02-24 19:18:47 +0300

    bugfix parser

--- tester/test1.py
+++ tester/test1.py
@@ -13,7 +13,34 @@ class AvailableTester:
     def get_data(self, urls: list) -> list:
         data = []
         for url in urls:
-            signature = requests.get("https://2ip.ru/site-availability/").text
+            cookies = {
+                'dfjoCvIgkbblF5DJfa551AokP0o': 'igDggjMBG197PLliVZWgCW-B7R0',
+                'LOW433wcIwWHRSdBwilP9srZ5Do': '1677254738',
+                'w7uQRCdBmD7pu80CTALAL32VBhU': '1677341138',
+                'G2tBy3ekpeZO3qeyhUTDLpkQWZ8': 'h4h2AJeROItN7NP3IXASSaddpG4',
+                'PHPSESSID': 'go6m42natqlt0raf7lgn4rhdsn',
+            }
+
+            headers = {
+                'authority': '2ip.ru',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                'cache-control': 'max-age=0',
+                # 'cookie': 'dfjoCvIgkbblF5DJfa551AokP0o=igDggjMBG197PLliVZWgCW-B7R0; LOW433wcIwWHRSdBwilP9srZ5Do=1677254738; w7uQRCdBmD7pu80CTALAL32VBhU=1677341138; G2tBy3ekpeZO3qeyhUTDLpkQWZ8=h4h2AJeROItN7NP3IXASSaddpG4; PHPSESSID=go6m42natqlt0raf7lgn4rhdsn',
+                'referer': 'https://2ip.ru/site-availability/',
+                'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': '"Windows"',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?1',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+            }
+
+            signature = requests.get('https://2ip.ru/site-availability/',
+                                    cookies=cookies, headers=headers).text
             signature = signature.split("var services = [")[1].split("{")[1][:-6]. \
                 replace("\n", "").replace("\t", " ").replace("     ", ""). \
                 split("\'")[1].split("\'")[0].split("/?")[1]
